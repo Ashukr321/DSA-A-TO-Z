51225defdb6b191d0fbb7d4f8a28728d12d9e8fd
Ashukr321 <coderashukr321@gmail.com>
2023-11-28 21:44:52 +0530

    concept is completed

--- prpgramming_fundamental/intro.cpp
+++ prpgramming_fundamental/intro.cpp
@@ -12,3 +12,37 @@ int main(){
   cout<<"welcome to the dsa a to z"<<endl;
   return 0; 
 }
+// graphical representaion of the alogorithms is know as  flowchart 
+// oval is use for the start and end 
+// parallelograme is use for the input and output 
+// rect is  use for the processing 
+// rhombus is use for the decision making
+
+
+// pseduo code 
+//  details info about algo  or we can say just  for the 
+// approch to solve in textual form 
+
+
+// exmaple
+
+// add two number 
+// diff two number 
+// mul two number 
+// avg two number 
+// circumference of the circle 
+// check number is even or odd 
+// student grade system 
+// check the number is positive negative or zero 
+// find perimeter of triangle 
+// find simple interest 
+// find factorial of number 
+// check prime or not 
+// valid triangle or not 
+// print n to n but only even number 
+// print max of three number 
+// printing counting 1 to n
+// add n numbers from users 
+// print 1 to n only odd number 
+
+
