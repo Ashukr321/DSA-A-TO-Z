09dd26af6285e233710130431f3015d5fc292da2
Ashukr321 <coderashukr321@gmail.com>
2023-11-28 21:01:23 +0530

    intro to dsa

---
+++ intro.cpp
@@ -0,0 +1,5 @@
+// Approch to solve the problem 
+// step1 analyze the problem 
+// step2 i/p value and  check test case 
+// step3 approch to solve the problem 
+// implementation of the alog , using high level language 
